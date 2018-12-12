cfdac84e37cef301037c8c20c306e66583f3c040
Микола Бондарчук <bondarchuk_ak13@nuwm.edu.ua>
2018-12-12 20:32:28 +0300

    add loading event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loading = true;
 
-  test() {
-    this.loading = !this.loading;
-
+  ngOnInit() {
+    this.loading = false;
   }
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -2,9 +2,10 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
-  imports: [MatIconModule, MatButtonModule, MatCheckboxModule, MatToolbarModule],
-  exports: [MatIconModule, MatButtonModule, MatCheckboxModule, MatToolbarModule]
+  imports: [MatProgressSpinnerModule, MatIconModule, MatButtonModule, MatCheckboxModule, MatToolbarModule],
+  exports: [MatProgressSpinnerModule, MatIconModule, MatButtonModule, MatCheckboxModule, MatToolbarModule]
 })
 export class MaterialModule { }
